0f33ec67a56fabc9868f70e0f56f2e59413dda35
a465858647 <465858647@qq.com>
2020-06-08 17:35:45 +0800

    'xiugai'

--- JS/main.js
+++ JS/main.js
@@ -6,11 +6,17 @@ require.config({
 		cesium: '../node_modules/Cesium/Cesium',
 		initmap: 'hnbd/hnbd_initmap',
 		layers: 'hnbd/hnbd_layers',
+		// tianditu: 'http://api.tianditu.gov.cn/cdn/plugins/cesium/cesiumTdt',
 	},
 });
 var viewer;
+var token = '418a00e90328d5f937d647feaa4eedbf';
+var tdtUrl = 'https://t{s}.tianditu.gov.cn/';
+var subdomains = ['0', '1', '2', '3', '4', '5', '6', '7'];
 require(['jquery'], function () {
 	require(['bootstrap'], function () {});
 });
 require(['initmap']);
-require(['layers']);
+// require(['http://api.tianditu.gov.cn/cdn/plugins/cesium/cesiumTdt.js'], function () {
+// 	require(['layers']);
+// });
