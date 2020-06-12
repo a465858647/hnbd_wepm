e3bfc91f7477de88f6fd04ddb2464884bb2f358d
a465858647 <465858647@qq.com>
2020-06-12 17:22:52 +0800

    ''

--- JS/hnbd/hnbd_initmap.js
+++ JS/hnbd/hnbd_initmap.js
@@ -59,7 +59,20 @@ define(['cesium', 'jquery'], function (Cesium, $) {
 		});
 	});
 	// //导航栏
-	// viewer.extend(Cesium.viewerCesiumNavigationMixin, {});
+	// require(['../node_modules/cesium-navigation-es6-master/viewerCesiumNavigationMixin.js', 'cesium'], function (CesiumNavigation, Cesium) {
+	// 	var options = {};
+	// 	// 用于在使用重置导航重置地图视图时设置默认视图控制。接受的值是Cesium.Cartographic 和 Cesium.Rectangle.
+	// 	options.defaultResetView = Cesium.Rectangle.fromDegrees(80, 22, 130, 50);
+	// 	// 用于启用或禁用罗盘。true是启用罗盘，false是禁用罗盘。默认值为true。如果将选项设置为false，则罗盘将不会添加到地图中。
+	// 	options.enableCompass = true;
+	// 	// 用于启用或禁用缩放控件。true是启用，false是禁用。默认值为true。如果将选项设置为false，则缩放控件将不会添加到地图中。
+	// 	options.enableZoomControls = true;
+	// 	// 用于启用或禁用距离图例。true是启用，false是禁用。默认值为true。如果将选项设置为false，距离图例将不会添加到地图中。
+	// 	options.enableDistanceLegend = true;
+	// 	// 用于启用或禁用指南针外环。true是启用，false是禁用。默认值为true。如果将选项设置为false，则该环将可见但无效。
+	// 	options.enableCompassOuterRing = true;
+	// 	CesiumNavigation(viewer, options);
+	// });
 
 	//控制视角不转到地下
 	$(function () {
