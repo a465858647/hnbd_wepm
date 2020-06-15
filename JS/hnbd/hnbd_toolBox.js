e549b784eda53abcedbc7f37d4d8f6a07c86d63e
a465858647 <465858647@qq.com>
2020-06-15 16:52:17 +0800

    '开始做距离测量'

--- JS/hnbd/hnbd_toolBox.js
+++ JS/hnbd/hnbd_toolBox.js
@@ -1,46 +1,9 @@
 define(['Cesium', 'jquery'], function (Cesium, $) {
 	$('#surveyCoordinate').on('click', function (event) {
-		// surveyCoordinate(event, id);
 		surveyCoordinate(event);
 		deleteCoordinateLabel(event);
 	});
 	function surveyCoordinate(event) {
-		// var handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas);
-		// //todo：在椭球下点击创建点
-		// handler.setInputAction(function (event) {
-		// 	var cartesian = viewer.camera.pickEllipsoid(event.position, viewer.scene.globe.ellipsoid); //返回在椭球上面的点的坐标
-		// 	if (Cesium.defined(cartesian)) {
-		// 		console.log(cartesian);
-		// 		//将笛卡尔三维坐标转为地图坐标（弧度）
-		// 		var cartographic = viewer.scene.globe.ellipsoid.cartesianToCartographic(cartesian);
-		// 		console.log(cartographic);
-		// 		//将地图坐标（弧度）转为十进制的度数
-		// 		var lat_String = Cesium.Math.toDegrees(cartographic.latitude).toFixed(4);
-		// 		var log_String = Cesium.Math.toDegrees(cartographic.longitude).toFixed(4);
-		// 		var alti_String = (viewer.camera.positionCartographic.height / 1000).toFixed(2);
-		// 		console.log(log_String);
-		// 		console.log(lat_String);
-		// 		console.log(alti_String);
-		// 		//添加标注点
-		// 		viewer.entities.add({
-		// 			id: id,
-		// 			name: name,
-		// 			// position: cartesian,
-		// 			position: Cesium.Cartesian3.fromDegrees(log_String, lat_String),
-		// 			point: { pixelSize: 5, color: Cesium.Color.RED, outlineColor: Cesium.Color.WHITE, outlineWidth: 1 },
-		// 			//字体标签样式
-		// 			label: {
-		// 				text: '精度：' + log_String + '\n' + '纬度：' + lat_String + '\n' + '高程：' + alti_String,
-		// 				font: '12px Helvetica',
-		// 				fillColor: Cesium.Color.SKYBLUE,
-		// 				outlineColor: Cesium.Color.BLACK,
-		// 				outlineWidth: 2,
-		// 				style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-		// 			},
-		// 		});
-		// 		id = id + 1;
-		// 	}
-		// }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
 		var handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas);
 		if (Cesium.defined(viewer.terrainProvider.availability)) {
 			//todo：在显示地形情况下点击创建点
@@ -151,4 +114,133 @@ define(['Cesium', 'jquery'], function (Cesium, $) {
 			}
 		}, Cesium.ScreenSpaceEventType.RIGHT_CLICK);
 	}
+	$('#surveyDistance').on('click', function (event) {
+		// surveyDistance(event);
+		// deleteDistanceLabel(event);
+		// function surveyDistance(event) {
+		//   var handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas);
+		// }
+		// function deleteDistanceLabel(event) {}
+		//测量空间直线距离
+		/******************************************* */
+		var handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas);
+		function measureLineSpace(viewer, handler) {
+			// 取消双击事件-追踪该位置
+			viewer.cesiumWidget.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
+			// handler = new Cesium.ScreenSpaceEventHandler(viewer.scene._imageryLayerCollection);
+			var positions = [];
+			var poly = null;
+			var distance = 0;
+			var cartesian = null;
+			var floatingPoint;
+			handler.setInputAction(function (movement) {
+				let ray = viewer.camera.getPickRay(movement.endPosition);
+				cartesian = viewer.scene.globe.pick(ray, viewer.scene);
+				//cartesian = viewer.scene.camera.pickEllipsoid(movement.endPosition, viewer.scene.globe.ellipsoid);
+				if (positions.length >= 2) {
+					if (!Cesium.defined(poly)) {
+						poly = new PolyLinePrimitive(positions);
+					} else {
+						positions.pop();
+						// cartesian.y += (1 + Math.random());
+						positions.push(cartesian);
+					}
+					distance = getSpaceDistance(positions);
+					// console.log("distance: " + distance);
+					// tooltip.innerHTML='<p>'+distance+'米</p>';
+				}
+			}, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+
+			handler.setInputAction(function (movement) {
+				// tooltip.style.display = "none";
+				// cartesian = viewer.scene.camera.pickEllipsoid(movement.position, viewer.scene.globe.ellipsoid);
+				// cartesian = viewer.scene.pickPosition(movement.position);
+				let ray = viewer.camera.getPickRay(movement.position);
+				cartesian = viewer.scene.globe.pick(ray, viewer.scene);
+				if (positions.length == 0) {
+					positions.push(cartesian.clone());
+				}
+				positions.push(cartesian);
+				//在三维场景中添加Label
+				//   var cartographic = Cesium.Cartographic.fromCartesian(cartesian);
+				var textDisance = distance + '米';
+				// console.log(textDisance + ",lng:" + cartographic.longitude/Math.PI*180.0);
+				floatingPoint = viewer.entities.add({
+					name: '空间直线距离',
+					// position: Cesium.Cartesian3.fromDegrees(cartographic.longitude / Math.PI * 180, cartographic.latitude / Math.PI * 180,cartographic.height),
+					position: positions[positions.length - 1],
+					point: {
+						pixelSize: 5,
+						color: Cesium.Color.RED,
+						outlineColor: Cesium.Color.WHITE,
+						outlineWidth: 2,
+					},
+					label: {
+						text: textDisance,
+						font: '18px sans-serif',
+						fillColor: Cesium.Color.GOLD,
+						style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+						outlineWidth: 2,
+						verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+						pixelOffset: new Cesium.Cartesian2(20, -20),
+					},
+				});
+			}, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+			handler.setInputAction(function (movement) {
+				handler.destroy(); //关闭事件句柄
+				positions.pop(); //最后一个点无效
+				// viewer.entities.remove(floatingPoint);
+				// tooltip.style.display = "none";
+			}, Cesium.ScreenSpaceEventType.RIGHT_CLICK);
+
+			var PolyLinePrimitive = (function () {
+				function _(positions) {
+					this.options = {
+						name: '直线',
+						polyline: {
+							show: true,
+							positions: [],
+							material: Cesium.Color.CHARTREUSE,
+							width: 10,
+							clampToGround: true,
+						},
+					};
+					this.positions = positions;
+					this._init();
+				}
+
+				_.prototype._init = function () {
+					var _self = this;
+					var _update = function () {
+						return _self.positions;
+					};
+					//实时更新polyline.positions
+					this.options.polyline.positions = new Cesium.CallbackProperty(_update, false);
+					viewer.entities.add(this.options);
+				};
+
+				return _;
+			})();
+
+			//空间两点距离计算函数
+			function getSpaceDistance(positions) {
+				var distance = 0;
+				for (var i = 0; i < positions.length - 1; i++) {
+					var point1cartographic = Cesium.Cartographic.fromCartesian(positions[i]);
+					var point2cartographic = Cesium.Cartographic.fromCartesian(positions[i + 1]);
+					/**根据经纬度计算出距离**/
+					var geodesic = new Cesium.EllipsoidGeodesic();
+					geodesic.setEndPoints(point1cartographic, point2cartographic);
+					var s = geodesic.surfaceDistance;
+					//console.log(Math.sqrt(Math.pow(distance, 2) + Math.pow(endheight, 2)));
+					//返回两点之间的距离
+					s = Math.sqrt(Math.pow(s, 2) + Math.pow(point2cartographic.height - point1cartographic.height, 2));
+					distance = distance + s;
+				}
+				return distance.toFixed(2);
+			}
+		}
+		measureLineSpace(viewer, handler);
+	});
 });
